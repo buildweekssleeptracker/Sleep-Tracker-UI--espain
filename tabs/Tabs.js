ccab24ff6e94cdd5ee7e37244668a7377384d9f7
Elise Spain <elisespain@gmail.com>
2019-05-23 15:48:27 -0700

    updates to about page tabs

---
+++ tabs/Tabs.js
@@ -0,0 +1,46 @@
+class TabLink {
+    constructor(tabGoals){
+        this.tabGoals = tabGoals;
+        this.tabData = this.tabGoals.dataset.tab;
+    }
+    if(this.tabData === 'all'){
+        this.cards = document.querySelectorAll('.card');
+    } else {
+        this.cards = document.querySelectorAll(`.card[data-tab=${this.tabData}]`);
+    }
+    this.cards = Array.from(this.cards).map(card => new TabCard(card));
+    this.tabGoals.addEventListener("click", ()=>this.selectTab());
+
+}
+
+selectTab(){
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => tab.classList.remove('active-tab'));
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => card.getElementsByClassName.display = 'none');
+    this.tabGoals.classList.add('active-tab');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// STart of the tabs Section 
+let tabs = document.querySelectorAll(".tab").forEach(tab => new TabLink (tab));
