5b9d41be6a6fd88a08cd449650a8309be8407938
Elise Spain <elisespain@gmail.com>
2019-05-23 18:58:12 -0700

    about page titles and photos added

--- tabs/Tabs.js
+++ tabs/Tabs.js
@@ -2,37 +2,38 @@ class TabLink {
     constructor(tabGoals){
         this.tabGoals = tabGoals;
         this.tabData = this.tabGoals.dataset.tab;
-    }
-    if(this.tabData === 'all'){
+    
+        if(this.tabData === 'all'){
         this.cards = document.querySelectorAll('.card');
-    } else {
+        } else {
         this.cards = document.querySelectorAll(`.card[data-tab=${this.tabData}]`);
+        }
+        this.cards = Array.from(this.cards).map(card => new TabCard(card));
+        this.tabGoals.addEventListener("click", ()=>this.selectTab());
+
     }
-    this.cards = Array.from(this.cards).map(card => new TabCard(card));
-    this.tabGoals.addEventListener("click", ()=>this.selectTab());
 
-}
 
-selectTab(){
+    selectTab(){
     const tabs = document.querySelectorAll('.tab');
     tabs.forEach(tab => tab.classList.remove('active-tab'));
     const cards = document.querySelectorAll('.card');
     cards.forEach(card => card.getElementsByClassName.display = 'none');
     this.tabGoals.classList.add('active-tab');
+    this.cards.forEach(card => card.selectCard());
+
+    }
+
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+class TabCard {
+  constructor(cardGoals){
+    this.cardGoals = cardGoals;  
+  } 
+  selectCard(){
+     this.cardGoals.style.display = "flex"; 
+  } 
+}
 
 
 
